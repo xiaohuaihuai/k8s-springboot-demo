f0f21b6d9bc1c1e5c4f62a5bb5c420f09a740357
yangqiuhua <1134903504@qq.com>
2019-12-17 17:04:03 +0800

    测试volume

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,11 @@
-FROM maven:3.5.4-jdk-8 AS mvnBuild
+FROM maven:3.5.4-jdk-8-alpine AS mvnBuild
+VOLUME /data/k8s-springboot-demo
 ADD ./src src/
 ADD ./pom.xml pom.xml
 RUN mvn clean package -Dmaven.test.skip=true
 
 FROM java:8
-VOLUME /tmp
-COPY --from=mvnBuild target/k8s-springboot-demo-0.0.1-SNAPSHOT.jar k8s-springboot-demo.jar
+VOLUME /data/k8s-springboot-demo
+COPY --from=mvnBuild target/k8s-springboot-demo.jar k8s-springboot-demo.jar
+RUN bash -c 'touch k8s-springboot-demo.jar'
 ENTRYPOINT ["java","-jar","k8s-springboot-demo.jar"]
--- src/main/java/com/example/k8s/springboot/demo/HelloController.java
+++ src/main/java/com/example/k8s/springboot/demo/HelloController.java
@@ -1,14 +1,23 @@
 package com.example.k8s.springboot.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    public static final Logger logger = LoggerFactory.getLogger(HelloController.class);
 
-    @RequestMapping("/hello")
-    public String hello(){
-        return "hello world";
+    @RequestMapping("/hello/{name}")
+    public String hello(@PathVariable String name){
+        logger.info("info name:{}", name);
+        logger.error("error name:{}", name);
+        logger.debug("debug name:{}", name);
+        logger.warn("warn name:{}", name);
+        logger.trace("trace name:{}", name);
+        return "hello,"+name;
     }
 
 }
